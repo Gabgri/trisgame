3abeefeb996074accb1990d7f5e60c3f231e2d0e
Gabriele <gabgri14@gmail.com>
2024-07-13 09:17:29 +0200

    x&o-TrisSvg

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
-  <h1 class="text-3xl font-bold">TrisGame</h1>
+  <h1 class="mb-10 text-3xl font-bold">TrisGame</h1>
+
   <button class="bg-slate-200 text-gray-800 font-bold rounded border-b-2 border-blue-500 hover:border-blue-600 hover:bg-blue-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center"
   v-show="!isStarted" @click="startGame">Start Your Game!</button>
+
   <button class="bg-slate-200 text-gray-800 font-bold rounded border-b-2 border-blue-500 hover:border-blue-600 hover:bg-blue-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center"
   v-show="isStarted">Reset</button>
-  <p>{{ input }} has won the Game</p>
+
+  <p>{{ opath }} has won the Game</p>
   <Grid v-if="isStarted" />
 </template>
 
--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,29 +1,54 @@
 <template>
     <!--Grid-->
-    <div> 
-        <p>Sono dentro il template "grid"</p>
+    <div class="mx-auto mt-10 max-w-md"> 
         <!--Rows-->
-        <div> 
-            <p>Riga 1</p>
-            <Box />
-            <Box />
-            <Box />
+        <div class="grid grid-cols-3"> 
+            <div>{{ input }}</div>
+            <div class="border-x-2 border-gray-700">
+                {{ input }}</div>
+            <img src="../assets/oTris.svg" alt="logo">
+            <!-- <div>{{ input }}</div> -->
         </div>
-        <div> 
-            <p>Riga 2</p>
-            <Box />
-            <Box />
-            <Box />
+        <div class="grid grid-cols-3"> 
+            <div class="border-y-2 border-gray-700">
+                {{ input }}</div>
+            <div class="border-2 border-gray-700">
+                {{ input }}</div>
+            <div class="border-y-2 border-gray-700">
+                <img src="../assets/oTris.svg" alt="logo"></div>
         </div>
-        <div> 
-            <p>Riga 3</p>
-            <Box />
-            <Box />
-            <Box />
+        <div class="grid grid-cols-3"> 
+            <div>{{ input }}</div>
+            <div class="border-x-2 border-gray-700">
+                {{ input }}</div>
+            <!-- <div>{{ input }}</div> -->
+            <img src="../assets/oTris.svg" alt="logo">
         </div>
     </div>
+
+
+    <!-- <div class="mx-10 mt-20 "> 
+
+        <div class="flex justify-center"> 
+            <div>{{ input }}</div>
+            <div class="border-x-2 border-gray-700"><p>Riga 1</p></div>
+            <div><p>Riga 1</p></div>
+        </div>
+        <div class="flex justify-center"> 
+            <div class="border-y-2 border-gray-700"><p>Riga 2</p></div>
+            <div class="border-2 border-gray-700"><p>Riga 2</p></div>
+            <div class="border-y-2 border-gray-700"><p>Riga 2</p></div>
+        </div>
+        <div class="flex justify-center"> 
+            <div><p>Riga 3</p></div>
+            <div class="border-x-2 border-gray-700"><p>Riga 3</p></div>
+            <div><p>Riga 3</p></div>
+        </div>
+    </div>  -->
 </template>
 
 <script setup>
-import Box from './Box.vue';
+  const input = false;
+  const output = 'x';
+
 </script>
