a05320d7f07f3351655f21560415b634df0c8117
Gabriele <gabgri14@gmail.com>
2024-07-15 10:55:54 +0200

    V-for in Grid and getStyle

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,9 +1,15 @@
 <template>
     <!--Grid-->
-    <div id="grid" class="grid grid-cols-3 sm:max-w-md md:max-w-lg lg:max-w-xl"> 
+    <div id="grid" class="grid grid-cols-3 sm:max-w-md md:max-w-lg lg:max-w-xl
+    "> 
 
         <!--Rows-->
-        <Box @click.once="handler"
+        <Box v-for="i in 9" :key="i" 
+        @click.once="handler" :trueOrfalse="state"
+        class="h-32" :class="getStyle(i)"></Box>
+    
+
+        <!-- <Box @click.once="handler"
         :trueOrfalse="state" class="h-32"/>
         <Box @click.once="handler"
         :trueOrfalse="state"
@@ -27,19 +33,37 @@
         :trueOrfalse="state"
         class="border-x-2 border-gray-700"/>
         <Box @click.once="handler"
-        :trueOrfalse="state"/>
+        :trueOrfalse="state"/> -->
 
     </div>
 </template>
 
 <script setup>
+import { Result } from "postcss";
 import Box from "./Box.vue";
-import { ref } from "vue";
+import { reactive, ref } from "vue";
 
 const state = ref(true);
 
+const style = reactive({
+  sideX: 'border-x-2 border-gray-700',
+  sideY: 'border-y-2 border-gray-700',
+  bothSide: 'border-2 border-gray-700'
+})
+
+function getStyle(i) {
+  if (i == 2) {
+    return style.sideX;
+  } else if (i == 4 | i == 6) {
+    return style.sideY;
+  } else if (i == 5) {
+    return style.bothSide;
+  } else if (i == 8) {
+    return style.sideX;
+  }
+}
+
 function handler() {
-  console.log(state.value);
   state.value = !state.value;
 }
 </script>
