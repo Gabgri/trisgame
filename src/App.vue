54cf60c64c08a303ffe7cd0503a16fcb5d20a890
Gabriele <83372093+Gabgri@users.noreply.github.com>
2024-09-21 18:41:38 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,7 @@
-<template>
-  <h1 class="mt-10 mb-4 text-4xl font-bold">TrisGame</h1>
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';  
+import './assets/main.css';  
 
-  <div class="flex justify-center my-10">
-    <button v-show="!isStarted" @click="startGame" 
-    class="self-center text-xl font-bold bg-slate-200 text-gray-800 rounded border-b-2 border-blue-500 hover:border-blue-600 hover:bg-blue-500 hover:text-white shadow-md py-2 w-60">
-    Start Your Game!</button>
+createApp(App).use(router).mount('#app');
 
-    <button v-show="isStarted" @click="startGame"
-    class="text-xl font-bold bg-slate-200 text-gray-800 rounded border-b-2 border-blue-500 hover:border-blue-600 hover:bg-blue-500 hover:text-white shadow-md py-2 w-60">
-    Reset</button>
-  </div>
-
-  <Grid v-if="isStarted"/>
-</template>
-
-<script setup>
-import Grid from "./components/Grid.vue";
-import { ref } from "vue";
-
-  const isStarted = ref(false);
-  function startGame() {
-    isStarted.value = !isStarted.value;
-  }
-</script>
